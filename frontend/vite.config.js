ef437685975a3fc45408f5b44d503e1972eb5a2f
Francis Damachi <damachi49@gmail.com>
2025-06-01 00:33:57 +0200

    deploy test2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,28 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// // https://vite.dev/config/
+// export default defineConfig(({ command }) => ({
+//   base: command === "build" ? "/static/" : "/",
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/static/" : "/",
+export default defineConfig({
+  base: "./", // Ensures relative paths in production
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
